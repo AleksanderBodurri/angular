54b2d4a3b88555f643a8adb986aa6c629b09656e
Milo <mmighdoll@google.com>
2025-04-07 16:42:45 +0000

    WIP: add stable id

--- devtools/projects/ng-devtools-backend/src/lib/client-event-subscribers.ts
+++ devtools/projects/ng-devtools-backend/src/lib/client-event-subscribers.ts
@@ -572,5 +572,14 @@ const getSignalGraphCallback = (messageBus: MessageBus<Events>) => (element: Ele
   }
 
   const graph = ng.ɵgetSignalGraph?.(injector);
-  if (graph) messageBus.emit('latestSignalGraph', [graph]);
+  if (graph) {
+    const nodes = graph.nodes.map((node) => {
+      return {
+        id: node.id,
+        kind: node.kind,
+        label: node.label,
+      };
+    })
+    messageBus.emit('latestSignalGraph', [graph]);
+  }
 };
